a7c1bd590747c0bdae3c1f27da86b15ace49bebf
최성재 <sungjaechoi39@gmail.com>
2024-01-26 09:51:39 +0900

    미들웨어 튜토리얼 환경 설정

---
+++ src/containers/CounterContainer.js
@@ -0,0 +1,27 @@
+import { connect } from "react-redux";
+import { increase, decrease } from "../modules/counter";
+import Counter from "../components/Counter";
+
+const CounterContainer = ({number, increase, decrease}) => {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease}/>
+  );
+};
+
+// const mapStateToProps = state => state.Counter.number
+// const mapDipatchToProps = dispatch => ({
+//   increase,
+//   decrease
+// })
+
+// export default connect(mapStateToProps, mapDipatchToProps)(CounterContainer)
+
+export default connect(
+  state => ({
+    number: state.counter
+  }),
+  {
+    increase,
+    decrease
+  }
+)(CounterContainer)
